56e636e45bbea1d3f24bc5e05e6057b6b773572c
Dudekula Rumina <137053135+dudekularumina@users.noreply.github.com>
2024-05-01 09:49:47 +0530

    Add files via upload

---
+++ jobfields.py
@@ -0,0 +1,12 @@
+def jobfields():
+    data={
+         'job_role':[],
+         'job_posted_on':[],
+         'job_location':[],
+         'job_link':[],
+         'employment_type':[],
+         'hourly_rate':[],
+         'job_description':[]
+
+    }
+    return data
